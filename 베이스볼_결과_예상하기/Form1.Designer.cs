d7cd4a834d7dc181affde006cbdfa0e7f1f7dccc
whdgns0332 <46296541+whdgns0332@users.noreply.github.com>
2020-10-27 00:50:25 +0900

    Add files via upload

---
+++ 베이스볼_결과_예상하기/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace 베이스볼_결과_예상하기
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AnswerRecordList = new System.Windows.Forms.ListBox();
+            this.PossibleAnswerList = new System.Windows.Forms.ListBox();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnRetry = new System.Windows.Forms.Button();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.LblMessage = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AnswerRecordList
+            // 
+            this.AnswerRecordList.FormattingEnabled = true;
+            this.AnswerRecordList.ItemHeight = 15;
+            this.AnswerRecordList.Location = new System.Drawing.Point(38, 39);
+            this.AnswerRecordList.Name = "AnswerRecordList";
+            this.AnswerRecordList.Size = new System.Drawing.Size(144, 364);
+            this.AnswerRecordList.TabIndex = 0;
+            // 
+            // PossibleAnswerList
+            // 
+            this.PossibleAnswerList.FormattingEnabled = true;
+            this.PossibleAnswerList.ItemHeight = 15;
+            this.PossibleAnswerList.Location = new System.Drawing.Point(610, 39);
+            this.PossibleAnswerList.Name = "PossibleAnswerList";
+            this.PossibleAnswerList.Size = new System.Drawing.Size(154, 364);
+            this.PossibleAnswerList.TabIndex = 1;
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(210, 321);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(98, 40);
+            this.BtnStart.TabIndex = 4;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnRetry
+            // 
+            this.BtnRetry.Enabled = false;
+            this.BtnRetry.Location = new System.Drawing.Point(350, 321);
+            this.BtnRetry.Name = "BtnRetry";
+            this.BtnRetry.Size = new System.Drawing.Size(98, 40);
+            this.BtnRetry.TabIndex = 4;
+            this.BtnRetry.Text = "Retry";
+            this.BtnRetry.UseVisualStyleBackColor = true;
+            this.BtnRetry.Click += new System.EventHandler(this.BtnRetry_Click);
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Location = new System.Drawing.Point(481, 321);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(98, 40);
+            this.BtnReset.TabIndex = 4;
+            this.BtnReset.Text = "Reset";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // LblMessage
+            // 
+            this.LblMessage.Location = new System.Drawing.Point(249, 378);
+            this.LblMessage.Name = "LblMessage";
+            this.LblMessage.Size = new System.Drawing.Size(291, 58);
+            this.LblMessage.TabIndex = 5;
+            this.LblMessage.Text = "숫자 버튼 세 개를 클릭하세요.";
+            this.LblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "시도한 번호 목록";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(649, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "정답 후보";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 453);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LblMessage);
+            this.Controls.Add(this.BtnReset);
+            this.Controls.Add(this.BtnRetry);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.PossibleAnswerList);
+            this.Controls.Add(this.AnswerRecordList);
+            this.Name = "Form1";
+            this.Text = "베이스볼";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox AnswerRecordList;
+        private System.Windows.Forms.ListBox PossibleAnswerList;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Button BtnRetry;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Label LblMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 베이스볼_결과_예상하기/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 베이스볼_결과_예상하기
+{
+    public partial class Form1 : Form
+    {
+        int[] TryNum = { 10, 10, 10 }; // 입력할 번호. 10은 할당되지 않은 상태
+        int[] AnsNum = new int[3]; // 정답
+        List<int[]> PossibleNum = new List<int[]> { }; // 답이 될 수 있는 숫자열 리스트
+        List<int[]> RecordNum = new List<int[]> { }; // 이전에 입력했던 숫자열 기록
+        List<int[]> RecordAns = new List<int[]> { }; // 각 Try마다 ball과 strike 기록
+        int ball = 0;
+        int strike = 0;
+        Button[] BtnNum = new Button[10]; // 입력 버튼 0~9
+        Label[] LblTry = new Label[3]; // 입력한 숫자 표시
+        int TryCount = 1; // 시도 횟수
+        bool isCorrect = false; // 정답을 맞추면 true
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 버튼 0~9까지 생성
+            for (int i = 0; i < 10; i++)
+            {
+                BtnNum[i] = new Button();
+                BtnNum[i].Font = new Font("굴림", 27F, FontStyle.Regular, GraphicsUnit.Point, 129);
+                BtnNum[i].Location = new Point(180 + 70 * (i - (i / 5) * 5), 100 + 70 * (i / 5));
+                BtnNum[i].Name = "button" + (i + 1).ToString();
+                BtnNum[i].Size = new Size(50, 50);
+                BtnNum[i].Text = (i + 1).ToString();
+                BtnNum[i].UseVisualStyleBackColor = true;
+                BtnNum[i].TextAlign = ContentAlignment.MiddleCenter;
+                BtnNum[i].Click += new EventHandler(button_Click);
+                Controls.Add(BtnNum[i]);
+            }
+            BtnNum[9].Text = "0";
+
+            // 입력한 숫자 표시 라벨 생성
+            for (int i = 0; i < 3; i++)
+            {
+                LblTry[i] = new Label();
+                LblTry[i].BackColor = SystemColors.ControlLightLight;
+                LblTry[i].BorderStyle = BorderStyle.FixedSingle;
+                LblTry[i].Font = new Font("굴림", 28.2F, FontStyle.Regular, GraphicsUnit.Point, 129);
+                LblTry[i].Location = new Point(220 + i*100, 30);
+                LblTry[i].Name = "TryNum" + (i+1).ToString();
+                LblTry[i].Size = new Size(52, 52);
+                LblTry[i].Text = "";
+                LblTry[i].TextAlign = ContentAlignment.MiddleCenter;
+                Controls.Add(LblTry[i]);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            do // 정답 설정
+            {
+                for (int i = 0; i < 3; i++)
+                    AnsNum[i] = r.Next(9);
+            }
+            while (AnsNum[0] == AnsNum[1] || AnsNum[1] == AnsNum[2] || AnsNum[0] == AnsNum[2]);
+            // 세 숫자 모두 다를 때까지 반복
+
+            for (int i = 0; i < 10; i++) // 가능한 모든 번호 리스트 생성 ([0, 0, 0] ~ [9, 9, 9])
+                for (int j = 0; j < 10; j++)
+                    for (int k = 0; k < 10; k++)
+                    {
+                        if (i != j && j != k && k != i) // 세 개의 숫자가 모두 다르면 리스트에 추가
+                        {
+                            int[] arr = { i, j, k };
+                            PossibleNum.Add(arr);
+                            PossibleAnswerList.Items.Add("[" + arr[0] + ", " + arr[1] + ", " + arr[2] + "]");
+                        }
+                    }
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            if (TryNum.Contains(10)) // 값이 하나라도 입력이 안되있으면
+            {
+                LblMessage.Text = "세 버튼 모두 클릭하세요.";
+                return;
+            }
+
+            foreach (int[] arr in RecordNum) // 이전에 입력했던 숫자열인지 확인
+            {
+                int isMatch = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    if (arr[i] != TryNum[i])
+                        break;
+                    isMatch++;
+                }
+                if (isMatch == 3)
+                {
+                    LblMessage.Text = "이전에 입력했던 숫자열 입니다.";
+                    foreach (Button btn in BtnNum)
+                        btn.Enabled = true;
+                    TryNum[0] = TryNum[1] = TryNum[2] = 10;
+                    LblTry[0].Text = LblTry[1].Text = LblTry[2].Text = "";
+                    return;
+                }
+            }
+
+            // 통상 분기 
+            BtnStart.Enabled = false;
+            BtnRetry.Enabled = true;
+            BtnReset.Enabled = false;
+
+            // strike와 ball 계산
+            for (int i = 0; i < 3; i++)
+            {
+                if (AnsNum.Contains(TryNum[i])) // strike + ball
+                    ball += 1;
+                if (AnsNum[i] == TryNum[i])
+                    strike += 1;
+            }
+            ball -= strike;
+
+            if (strike == 3)
+            {
+                LblMessage.Text
+                    = "정답입니다. " + TryCount.ToString() + "번 만에 맞추셨습니다.\n다시 시작하려면 Retry를 누르세요";
+                isCorrect = true;
+            }
+            else
+            {
+                LblMessage.Text = ball.ToString() + "ball " + strike.ToString() + "strike 입니다.";
+                RecordNum.Add(new int[] { TryNum[0], TryNum[1], TryNum[2] });
+                // 주의 : 배열도 객체이므로 new로 새로 생성해야함. 그냥 TryNum을 대입하면 그 배열의 주소가 저장되서
+                //        TryNum의 값이 바뀌면 RecordNum의 배열 값도 바뀌게 됨.(사실 TryNum 그 자체임)
+            }
+
+            // 시도한 번호 목록에 기록 추가
+            AnswerRecordList.Items.Add(TryCount + "TRY : " + TryNum[0] + " " + TryNum[1] + " " + TryNum[2]);
+            AnswerRecordList.Items.Add(ball.ToString() + "ball " + strike.ToString() + "strike");
+            AnswerRecordList.Items.Add("");
+
+            RecordAns.Add(new int[] { ball, strike });
+
+            // 정답이 될 수 없는 숫자열 리스트 생성
+            ball = 0;
+            strike = 0;
+            List<int[]> WorngNum = new List<int[]> { }; // 답이 아닌 번호 목록
+            foreach (int[] arr in PossibleNum)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (arr.Contains(TryNum[i]))
+                        ball += 1;
+                    if (arr[i] == TryNum[i])
+                        strike += 1;
+                }
+                ball -= strike;
+
+                if (RecordAns[TryCount - 1][0] != ball || RecordAns[TryCount-1][1] != strike)
+                {
+                    WorngNum.Add(arr); // PossibleNum의 번호 중 답이 아닌 번호들 제외
+                }
+
+                ball = 0;
+                strike = 0;
+            }
+
+            // 후보 목록에 오답 리스트의 요소들 제거
+            foreach (int[] arr in WorngNum)
+            {
+                if (PossibleNum.Contains(arr))
+                {
+                    PossibleNum.Remove(arr);
+                }
+            }
+
+            // 리스트 박스 갱신
+            PossibleAnswerList.Items.Clear();
+            foreach (int[] arr in PossibleNum)
+            {
+                PossibleAnswerList.Items.Add("[" + arr[0] + ", " + arr[1] + ", " + arr[2] + "]");
+            }            
+        }
+
+        private void BtnRetry_Click(object sender, EventArgs e)
+        {
+            if (isCorrect == true) // 정답을 맞혔으면 모든 값 초기화
+            {
+                TryCount = 1;
+
+                foreach (Button btn in BtnNum)
+                    btn.Enabled = true;
+
+                TryNum[0] = TryNum[1] = TryNum[2] = 10;
+                LblTry[0].Text = LblTry[1].Text = LblTry[2].Text = "";
+
+                BtnReset.Enabled = true;
+                BtnStart.Enabled = true;
+                BtnRetry.Enabled = false;
+
+                AnswerRecordList.Items.Clear();
+                PossibleAnswerList.Items.Clear();
+
+                Random r = new Random();
+                do // 정답 설정
+                {
+                    for (int i = 0; i < 3; i++)
+                        AnsNum[i] = r.Next(9);
+                }
+                while (AnsNum[0] == AnsNum[1] || AnsNum[1] == AnsNum[2] || AnsNum[0] == AnsNum[2]);
+
+                PossibleNum.Clear();
+                for (int i = 0; i < 10; i++) // 가능한 모든 번호 리스트 생성
+                    for (int j = 0; j < 10; j++)
+                        for (int k = 0; k < 10; k++)
+                        {
+                            if (i != j && j != k && k != i) // 세 개의 숫자가 모두 다르면 리스트에 추가
+                            {
+                                int[] arr = { i, j, k };
+                                PossibleNum.Add(arr);
+                                PossibleAnswerList.Items.Add("[" + arr[0] + ", " + arr[1] + ", " + arr[2] + "]");
+                            }
+                        }
+
+                RecordAns.Clear();
+                RecordNum.Clear();
+
+                LblMessage.Text = "숫자 버튼 세 개를 클릭하세요.";
+                isCorrect = false;
+            }
+            else
+            {
+                TryCount++;
+                foreach (Button btn in BtnNum)
+                    btn.Enabled = true;
+                TryNum[0] = TryNum[1] = TryNum[2] = 10;
+                LblTry[0].Text = LblTry[1].Text = LblTry[2].Text = "";
+                BtnReset.Enabled = true;
+                BtnStart.Enabled = true;
+                BtnRetry.Enabled = false;
+            }
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            TryNum[0] = TryNum[1] = TryNum[2] = 10;
+            LblTry[0].Text = LblTry[1].Text = LblTry[2].Text = "";
+            foreach (Button btn in BtnNum)
+            {
+                btn.Enabled = true;
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e) // 숫자 입력 버튼
+        {
+            Button button = sender as Button;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (TryNum[i] == 10)
+                {
+                    LblTry[i].Text = button.Text;
+                    TryNum[i] = int.Parse(button.Text);
+                    button.Enabled = false;
+                    break;
+                }
+            }
+        }
+    }
+}
