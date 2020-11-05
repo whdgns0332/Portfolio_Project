efb86f5a6c94c6b8ded5a4ca7fdc9c58efa97a18
whdgns0332 <46296541+whdgns0332@users.noreply.github.com>
2020-11-05 15:42:42 +0900

    Add files via upload

---
+++ NumberPad/NumPad.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace NumberPad
+{
+    partial class NumPad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(67, 393);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 28);
+            this.label1.TabIndex = 0;
+            // 
+            // NumPad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 468);
+            this.Controls.Add(this.label1);
+            this.Name = "NumPad";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.NumPad_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ NumberPad/NumPad.cs
@@ -0,0 +1,87 @@
+﻿using NumberPad;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberPad
+{
+    public partial class NumPad : Form
+    {
+        StringBuilder sb = new StringBuilder();
+        Button[] btn = new Button[10];
+        Main form1;
+
+        public NumPad(Main form)
+        {
+            InitializeComponent();
+            form1 = form;
+            ClientSize = new Size(340, 490);
+            label1.Location = new Point(10, 450);
+        }
+
+        private void NumPad_Load(object sender, EventArgs e)
+        {
+            // 0~9까지 버튼 생성
+            for (int i = 0; i < 10; i++)
+            {
+                btn[i] = new Button();
+                btn[i].Location = new Point(10 + 110 * (i % 3), 10 + 110 * (i / 3));
+                btn[i].Name = "button" + (i + 1).ToString();
+                btn[i].Size = new Size(100, 100);
+                btn[i].TabIndex = i;
+                btn[i].Text = (i + 1).ToString();
+                btn[i].UseVisualStyleBackColor = true;
+                btn[i].Click += new EventHandler(button_Click);
+                Controls.Add(btn[i]);
+            }
+            btn[9].Name = "button0";
+            btn[9].Location = new Point(120, 340);
+            btn[9].Text = "0";
+
+            // backspace 버튼 생성
+            Button Btn_bs = new Button();
+            Btn_bs.Location = new Point(10, 340);
+            Btn_bs.Name = "back_space";
+            Btn_bs.Size = new Size(100, 100);
+            Btn_bs.Text = "←";
+            Btn_bs.UseVisualStyleBackColor = true;
+            Btn_bs.Click += new EventHandler(button_Click_bs);
+            Controls.Add(Btn_bs);
+
+            // ok 버튼 생성
+            Button Btn_ok = new Button();
+            Btn_ok.Location = new Point(230, 340);
+            Btn_ok.Name = "OK";
+            Btn_ok.Size = new Size(100, 100);
+            Btn_ok.Text = "OK";
+            Btn_ok.UseVisualStyleBackColor = true;
+            Btn_ok.Click += new EventHandler(button_Click_ok);
+            Controls.Add(Btn_ok);
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            sb.Append(btn.Text);
+            label1.Text = sb.ToString();
+        }
+
+        private void button_Click_bs(object sender, EventArgs e)
+        {
+            sb.Remove(sb.Length - 1, 1);
+            label1.Text = sb.ToString();
+        }
+
+        private void button_Click_ok(object sender, EventArgs e)
+        {
+            form1.WriteTextBox(label1.Text);
+            Close();
+        }
+    }
+}
